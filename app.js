ff3924b6dcc032cc9547586dfc0266a5df881a9f
Vincent L <vincentluoster93@gmail.com>
2018-06-06 16:20:43 -0700

    Added controllers for app

---
+++ app.js
@@ -0,0 +1,16 @@
+// Modules for app
+//Budget Controller
+var budgetController = (function() {
+
+})();
+
+// UI Controller
+var UIController = (function() {
+
+})();
+
+// Global App Controller
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+})(budgetController, UIController)
