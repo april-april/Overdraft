7bd462886b49a63ced965fb1647fb7467a3b154d
Vincent L <vincentluoster93@gmail.com>
2018-06-06 17:34:02 -0700

    UI controller DOM

--- resources/JavaScript/app.js
+++ resources/JavaScript/app.js
@@ -8,6 +8,22 @@ var budgetController = (function() {
 // UI Controller
 var UIController = (function() {
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container',
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
+    };
+
 })();
 
 // Global App Controller
