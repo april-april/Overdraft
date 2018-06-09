de4ae197d95359726b90b27afbb5772c0172cc43
Vincent L <vincentluoster93@gmail.com>
2018-06-09 02:23:33 -0700

    update

--- resources/JavaScript/app.js
+++ resources/JavaScript/app.js
@@ -1,6 +1,4 @@
-// Modules for app
-//Budget Controller
-console.log('testing')
+// BUDGET CONTROLLER
 var budgetController = (function() {
     
     var Expense = function(id, description, value) {
@@ -18,7 +16,7 @@ var budgetController = (function() {
             this.percentage = -1;
         }
     };
-
+    //first
     
     Expense.prototype.getPercentage = function() {
         return this.percentage;
@@ -164,28 +162,11 @@ var budgetController = (function() {
             console.log(data);
         }
     };
+    
 })();
 
-// UI Controller
-var UIController = (function() {
 
-    var DOMstrings = {
-        inputType: '.add__type',
-        inputDescription: '.add__description',
-        inputValue: '.add__value',
-        inputBtn: '.add__btn',
-        incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list',
-        budgetLabel: '.budget__value',
-        incomeLabel: '.budget__income--value',
-        expensesLabel: '.budget__expenses--value',
-        percentageLabel: '.budget__expenses--percentage',
-        container: '.container',
-        expensesPercLabel: '.item__percentage',
-        dateLabel: '.budget__title--month'
-    };
 
-})();
 
 // UI CONTROLLER
 var UIController = (function() {
@@ -369,10 +350,12 @@ var UIController = (function() {
     
 })();
 
-// Global App Controller
 
+
+
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-
+    
     var setupEventListeners = function() {
         var DOM = UICtrl.getDOMstrings();
         
@@ -389,6 +372,85 @@ var controller = (function(budgetCtrl, UICtrl) {
         document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);        
     };
     
+    
+    var updateBudget = function() {
+        
+        // 1. Calculate the budget
+        budgetCtrl.calculateBudget();
+        
+        // 2. Return the budget
+        var budget = budgetCtrl.getBudget();
+        
+        // 3. Display the budget on the UI
+        UICtrl.displayBudget(budget);
+    };
+    
+    
+    var updatePercentages = function() {
+        
+        // 1. Calculate percentages
+        budgetCtrl.calculatePercentages();
+        
+        // 2. Read percentages from the budget controller
+        var percentages = budgetCtrl.getPercentages();
+        
+        // 3. Update the UI with the new percentages
+        UICtrl.displayPercentages(percentages);
+    };
+    
+    
+    var ctrlAddItem = function() {
+        var input, newItem;
+        
+        // 1. Get the field input data
+        input = UICtrl.getInput();        
+        
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            // 2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
+            // 3. Add the item to the UI
+            UICtrl.addListItem(newItem, input.type);
+
+            // 4. Clear the fields
+            UICtrl.clearFields();
+
+            // 5. Calculate and update budget
+            updateBudget();
+            
+            // 6. Calculate and update percentages
+            updatePercentages();
+        }
+    };
+    
+    
+    var ctrlDeleteItem = function(event) {
+        var itemID, splitID, type, ID;
+        
+        itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+        
+        if (itemID) {
+            
+            //inc-1
+            splitID = itemID.split('-');
+            type = splitID[0];
+            ID = parseInt(splitID[1]);
+            
+            // 1. delete the item from the data structure
+            budgetCtrl.deleteItem(type, ID);
+            
+            // 2. Delete the item from the UI
+            UICtrl.deleteListItem(itemID);
+            
+            // 3. Update and show the new budget
+            updateBudget();
+            
+            // 4. Calculate and update percentages
+            updatePercentages();
+        }
+    };
+    
+    
     return {
         init: function() {
             console.log('Application has started.');
@@ -402,7 +464,8 @@ var controller = (function(budgetCtrl, UICtrl) {
             setupEventListeners();
         }
     };
+    
+})(budgetController, UIController);
 
-})(budgetController, UIController)
 
 controller.init();
